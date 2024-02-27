3e442e72c82e94dab44b47816b85173c6db80358
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-02-27 17:36:41 +0300

    Update about.py

--- about.py
+++ about.py
@@ -57,7 +57,7 @@ def app():
 
 
         st.subheader("John Ndelembi | Junior Data analyst ")
-        st.image("john.png")
+        st.image("9.PNG", width=450)
         st.markdown("""
                 B.A in Statistics | University of Dar es Salaaam
                 - Hands on experience in the field of data science.
