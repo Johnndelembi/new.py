c86a8a72838b275f16aa44bb8a876d1a93a807ba
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-03-14 00:44:58 +0300

    Update new.py

--- new.py
+++ new.py
@@ -13,9 +13,9 @@ st.set_page_config(
 
 page_bg_img = """
 <style>
-[data-testid="stAppViewContainer"] {
-background-image: url("https://images.unsplash.com/photo-1476673160081-cf065607f449?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-}
+#[data-testid="stAppViewContainer"] {
+#background-image: url("https://images.unsplash.com/photo-1476673160081-cf065607f449?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+#}
 
 [data-testid="stHeader"] {
 background-color: rgba(0, 0, 0, 0);
