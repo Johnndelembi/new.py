c8cc78375b8e178f22516893955fdab51a832d41
John Ndelembi <williamjohnie61@gmail.com>
2024-06-28 14:18:36 +0300

    Update about.py

--- about.py
+++ about.py
@@ -57,7 +57,7 @@ def app():
 
 
         st.subheader("John Ndelembi | Data analyst ")
-        st.image("IMG_7119.JPG", width=450)
+        st.image("mimi.png")
         st.markdown("""
                 B.A in Statistics | University of Dar es Salaaam
                 - Hands on experience in the field of data science.
